faf66e878974a9c14e80c880c910c0829bb281bb
Sam <sam@vividconcept.ai>
2025-02-24 12:56:09 -0800

    updating url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://main.d3d0lyiknk0flv.amplifyapp.com/',
+	site: 'https://tutorial.sam.land',
 	integrations: [mdx(), sitemap()],
 });
