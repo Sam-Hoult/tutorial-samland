2794e0e2349dacde1174e28af627b63128e03565
Sam <sam@vividconcept.ai>
2025-02-24 12:42:15 -0800

    updating about me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://main.d3d0lyiknk0flv.amplifyapp.com/',
 	integrations: [mdx(), sitemap()],
 });
